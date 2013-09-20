e6ba5650f29fef213331152eebe92fcb1f50eebb
David Potsiadlo <davidpots@gmail.com>
2013-09-20 11:36:21 -0400

    Removes the short URL hider

--- js/main.js
+++ js/main.js
@@ -312,9 +312,9 @@
                 }
             );
 
-            if (urlVars.numberOfGifs > 14) {
-              $('#get-short-url').hide();
-            }
+            // if (urlVars.numberOfGifs > 14) {
+            //   $('#get-short-url').hide();
+            // }
 
 // Copy to Clipboard plugin
             
